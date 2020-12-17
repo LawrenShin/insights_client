f21c6f77224de5ef14b3280b051403708dbd8009
Kirill <simplyforgit@gmail.com>
2020-12-17 18:45:01 +0300

    roots

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import rootSaga from './rootSaga';
+import rootReducer from "./rootReducer";
+
 
 const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  reducer,
+  rootReducer,
   applyMiddleware(sagaMiddleware)
 );
+
 sagaMiddleware.run(rootSaga);
