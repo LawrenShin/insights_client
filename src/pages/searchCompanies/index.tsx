452482dbc68ea5190c2d29625044de816f574142
Kirill <simplyforgit@gmail.com>
2021-05-17 14:59:34 +0300

    welcome back

--- src/pages/searchCompanies/index.tsx
+++ src/pages/searchCompanies/index.tsx
@@ -3,11 +3,14 @@ import LookupSearch from "../../components/lookupSearch";
 import CustomSearch from "../../components/customSearch";
 import Header from "../../components/Header";
 import useStyles from "./useStyles";
+import {useStyles as useWelcomeStyled} from '../../components/customSearch/useStyles';
+import Welcome from "../../components/forms/register/steps/welcome";
 
 
 const SearchCompanies = () => {
   const [tab, setTab] = useState<string>('company');
   const styles = useStyles();
+  const styles2 = useWelcomeStyled();
 
   return (
     <div className={styles.root}>
@@ -16,9 +19,10 @@ const SearchCompanies = () => {
         <div className={styles.searchContainer}>
           <LookupSearch tab={tab} setTab={setTab} />
         </div>
-        <div className={styles.searchSettingsContainer}>
+        <div className={`${styles.searchSettingsContainer} ${tab === 'company' ? styles.centerContent : ''}`}>
           {/* TODO: some day reconsider this cas custom might also be on companies */}
           {tab !== 'company' && <CustomSearch tab={tab} setTab={setTab} />}
+          {tab === 'company' && <Welcome classes={styles2} />}
         </div>
       </div>
     </div>
